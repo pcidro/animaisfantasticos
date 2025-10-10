af405b926a0f95a92578bb0eda01153d61487312
Paulo Cidro <paulocidro@gmail.com>
2025-10-10 08:23:42 -0300

    tooltip new

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import TabNav from "./modules/tabnav.js";
 import Modal from "./modules/modal.js";
-import initTooltip from "./modules/tooltip.js";
+import Tooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
@@ -27,6 +27,10 @@ const modal = new Modal(
 );
 
 modal.init();
+
+const tooltip = new Tooltip("[data-tooltip]");
+
+tooltip.init();
 
 initTooltip();
 initDropdownMenu();
